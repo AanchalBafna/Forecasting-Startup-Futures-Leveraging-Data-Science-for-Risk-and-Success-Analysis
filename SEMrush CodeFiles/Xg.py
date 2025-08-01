ed382e9467d9d2e144200169afccaf303329ef02
Aanchal Bafna <aanchalbafna25@gmail.com>
2025-08-01 15:22:30 +0530

    Update Xg.py

--- SEMrush CodeFiles/Xg.py
+++ SEMrush CodeFiles/Xg.py
@@ -15,9 +15,11 @@ from sklearn.metrics import (
 )
 
 # === Paths ===
-file_path = r"C:\Users\aanch\Downloads\Startup\Data\web_metrics_standardized_v3.csv"
-model_path = r"C:\Users\aanch\Downloads\Startup\SEm\pickel\xgboost_web_model.pkl"
-plot_dir = os.path.join(os.getcwd(), "Plots")
+file_path = "Dataset/web_metrics_standardized_v3.csv"
+model_path = "SEMrush CodeFiles/pickel/catboost_balanced_model.pkl"
+
+# Plot saving directory
+plot_dir = os.path.join(os.getcwd(), "SEMrush CodeFiles/plots/XGboost Plots")
 os.makedirs(os.path.dirname(model_path), exist_ok=True)
 os.makedirs(plot_dir, exist_ok=True)
 
