18bb7b42b48c6dbdb1973f9c68600881aefff689
Aanchal Bafna <aanchalbafna25@gmail.com>
2025-08-01 13:48:17 +0530

    Update Cat.py

--- CrunchbaseCodeFiles/Cat.py
+++ CrunchbaseCodeFiles/Cat.py
@@ -13,8 +13,8 @@ import seaborn as sns
 import pickle
 
 # Paths
-file_path = r"Dataset/updated_startup_data.csv"
-model_path = r"C:\Users\aanch\Downloads\Startup\Crunch\pickel\cat.pkl"
+file_path = "Dataset/updated_startup_data.csv"
+model_path = "CrunchbaseCodeFiles/pickel/cat.pkl"
 plot_dir = r"C:\Users\aanch\Downloads\Startup\Crunch\plots\Cat"
 os.makedirs(plot_dir, exist_ok=True)
 
